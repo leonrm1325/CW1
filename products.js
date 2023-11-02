86983e5ebbf11fe296e26ef3ff144d79971b2ec4
Leon Martin <leonreecemartin@gmail.com>
2023-11-02 17:51:59 +0000

    added images, changed data so they are stored in a js file .

---
+++ products.js
@@ -0,0 +1,19 @@
+let products = [
+    {
+        id:1,
+        title:'Maths class',
+        image: 'images/mathsIcon.png',
+        description:'After school maths class to help students strengthen their maths skills',
+        availability:5,
+        price:15,
+    },
+
+    {
+        id:2,
+        title:'English class',
+        image: 'images/englishIcon.png',
+        description:'After school english class to help students strengthen their english skills',
+        availability:5,
+        price:15,
+    },
+]

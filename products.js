ba56ef4c33e06c41da11f3a4521d64d1b45d6d8a
Leon Martin <leonreecemartin@gmail.com>
2023-11-02 18:14:45 +0000

    10 clubs/lessons available

--- products.js
+++ products.js
@@ -1,10 +1,12 @@
+//Array of products data to be read and displayed into the main html file
+
 let products = [
     {
         id:1,
         title:'Maths class',
         image: 'images/mathsIcon.png',
         description:'After school maths class to help students strengthen their maths skills',
-        availability:5,
+        availability:10,
         price:15,
     },
 
@@ -13,7 +15,79 @@ let products = [
         title:'English class',
         image: 'images/englishIcon.png',
         description:'After school english class to help students strengthen their english skills',
-        availability:5,
+        availability:10,
         price:15,
+    },
+
+    {
+        id:3,
+        title:'Football club',
+        image:'images/football.png',
+        description:'After school football to keep students active',
+        availability:15,
+        price:5,
+    },
+
+    {
+        id:4,
+        title:'Basketball club',
+        image:'images/basketball.png',
+        description:'After school basketball to keep students active',
+        availability:10,
+        price:5,
+    },
+
+    {
+        id:5,
+        title:'Tennis club',
+        image:'images/tennis.png',
+        description:'After school tennis to keep students active',
+        availability:8,
+        price:5,
+    },
+
+    {
+        id:6,
+        title:'Debate club',
+        image:'images/debate.png',
+        description:'After school debating on the current world issues',
+        availability:8,
+        price:6,
+    },
+
+    {
+        id:7,
+        title:'Football club',
+        image:'images/filmClub.png',
+        description:'After school film club watching, reviewing and discussing a variety of movies',
+        availability:15,
+        price:10,
+    },
+
+    {
+        id:8,
+        title:'Science class',
+        image:'images/science.png',
+        description:'After school science class to help students strengthen their science skills',
+        availability:10,
+        price:15,
+    },
+
+    {
+        id:9,
+        title:'Spanish club',
+        image:'images/spanishIcon.png',
+        description:'After school spanish club for anyone interested in learning spanish',
+        availability:10,
+        price:4,
+    },
+
+    {
+        id:10,
+        title:'French club',
+        image:'images/frenchIcon.png',
+        description:'After school french club for anyone interested in learning french',
+        availability:10,
+        price:4,
     },
 ]
